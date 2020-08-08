100efd8875423f8cd66f41040bbd50a57f2c879a
Mateusz Janda <mateusz.janda@gmail.com>
2020-08-08 21:10:45 +0200

    Parse int

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 fn main() {
     println!("Hello, world!");
 
-    fun(b"asdf01234");
+//    fun(b"asdf");
 
+//    let s : String = String::new("text");
+
+//    bencode::Value::parse("l4:spam4:eggse");
+    bencode::Value::parse(b"i4e");
 }
 
 fn fun(a : &[u8]) {
+    println!("len {}", a.len());
     for (i, &v) in a.iter().enumerate() {
         println!("{}", v as char);
 
-        if v == b'f' {
-            fun1(&a[i..]);
+        if v == b'l' {
+            fun1(&a[i+1..]);
         }
     }
 }
 
 fn fun1(a : &[u8]) {
+
     println!("=====");
+    println!("len {}", a.len());
     for &v in a.iter() {
-        println!("{}", v as char);
+        println!("> {}", v as char);
     }
 }
+
+//impl String {
+//    fn fff(&self) {
+//        println!("asdf");
+//    }
+//}
 
 mod bencode {
     pub enum Value {
@@ -30,13 +43,33 @@ mod bencode {
 
     impl Value {
         pub fn parse(arg: &[u8]) {
-            for &b in arg.iter() {
+            for (i, &b) in arg.iter().enumerate() {
                 if b == b'i' {
-
+                    let len = Value::parse_int(&arg[i..]);
+                    println!("{}", len);
                 }
             }
 
         }
+
+        fn parse_int(arg : &[u8]) -> i32{
+
+            let mut len = Vec::new();
+
+            for (i, &b) in arg.iter().enumerate() {
+                if (b >= b'0' && b <= b'9') || b == b'-' {
+                    len.push(b);
+                } else if b == b'e' {
+                    break;
+                }
+            }
+
+//            println!(i);
+            let s = String::from_utf8(len).unwrap();
+            let l : i32 = s.parse().unwrap();
+            l
+        }
+
     }
 }
 
