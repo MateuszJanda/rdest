08be21b3da61182f235b0c21178ac3608fa24c83
Mateusz Janda <mateusz.janda@gmail.com>
2020-08-08 17:33:07 +0200

    Added module bencode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
 }
+
+mod bencode {
+    pub enum Value {}
+
+    impl Value {
+        pub fn parse(arg: &str) {
+            
+        }
+    }
+}
+
+//struct Bencode {
+//
+//}
+//
+//impl Bencode {
+//    fn parse(arg : &str) {
+//
+//    }
+//}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bencode::*;
+
+    #[test]
+    fn one_result() {
+        assert_eq!(Value::parse(""), ());
+    }
+}
