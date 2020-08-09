4e754f990b29609a81f60bb1dccd5fb8a5d85e24
Mateusz Janda <mateusz.janda@gmail.com>
2020-08-09 13:49:30 +0200

    Int parsing

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,54 @@
 fn main() {
     println!("Hello, world!");
 
-//    fun(b"asdf");
-
-//    let s : String = String::new("text");
-
-//    bencode::Value::parse("l4:spam4:eggse");
-//    bencode::Value::parse(b"i4e");
-    the_loop(b"asdf")
-}
-
-
-//fn fun(a : &[u8]) {
-//    println!("len {}", a.len());
-//    for (i, &v) in a.iter().enumerate() {
-//        println!("{}", v as char);
-//
-//        if v == b'l' {
-//            fun1(&a[i+1..]);
-//        }
-//    }
-//}
-//
-//fn fun1(a : &[u8]) {
-//
-//    println!("=====");
-//    println!("len {}", a.len());
-//    for &v in a.iter() {
-//        println!("> {}", v as char);
-//    }
-//}
-
-fn the_loop(arg : &[u8]) {
-    let mut it = arg.iter();
-    while let Some(v) = it.next() {
-        println!("{}", *v as char);
-        it.nth(0);
-    }
-}
-
-
-fn inc(arg : &[u8]) -> &[u8] {
-    let r = &arg[1..];
-//    println!("{}", r.len());
-    r
 
 }
+
 
 mod bencode {
+    #[derive(PartialEq, Debug)]
     pub enum Value {
         Int(i32),
-        List(),
+        Str(String),
+//        List(),
 //        Val(Value)
-        Val(Box<Value>)
+//        Val(Box<Value>)
     }
 
 
     impl Value {
-        pub fn parse(arg: &[u8]) {
-            for (i, &b) in arg.iter().enumerate() {
-                if b == b'i' {
-                    let len = Value::parse_int(&arg[i..]);
-                    println!("{}", len);
+        pub fn parse(arg: &[u8]) -> Vec<Value> {
+            let mut result = vec![];
+            let mut it = arg.iter();
+            while let Some(b) = it.next() {
+                if *b == b'i' {
+                    result.push(Value::parse_int(&mut it));
                 }
             }
 
+            result
         }
 
-        fn parse_int(arg : &[u8]) -> i32{
+        fn parse_int(it : &mut std::slice::Iter<u8>) -> Value {
+            let mut nums = vec![];
+            while let Some(b) = it.next() {
+                if (*b >= b'0' && *b <= b'9') || *b == b'-' {
+                    nums.push(*b);
+                } else if *b == b'e' {
+                    let nums_str = String::from_utf8(nums).unwrap();
+                    let int : i32 = nums_str.parse().unwrap();
+                    return Value::Int(int)
 
-            let mut len = Vec::new();
-
-            for (i, &b) in arg.iter().enumerate() {
-                if (b >= b'0' && b <= b'9') || b == b'-' {
-                    len.push(b);
-                } else if b == b'e' {
-                    break;
                 }
+
             }
 
+            Value::Int(0)
 //            println!(i);
-            let s = String::from_utf8(len).unwrap();
-            let l : i32 = s.parse().unwrap();
-            l
         }
 
     }
 }
-
-//struct Bencode {
-//
-//}
-//
-//impl Bencode {
-//    fn parse(arg : &str) {
-//
-//    }
-//}
 
 
 #[cfg(test)]
@@ -105,7 +57,22 @@ mod tests {
     use bencode::*;
 
     #[test]
-    fn one_result() {
-        assert_eq!(Value::parse(""), ());
+    fn empty_input() {
+        assert_eq!(Value::parse(b""), vec![]);
+    }
+
+    #[test]
+    fn positive_int() {
+        assert_eq!(Value::parse(b"i4e"), vec![Value::Int(4)]);
+    }
+
+    #[test]
+    fn negative_int() {
+        assert_eq!(Value::parse(b"i-4e"), vec![Value::Int(-4)]);
+    }
+
+    #[test]
+    fn two_ints() {
+        assert_eq!(Value::parse(b"i2ei-3e"), vec![Value::Int(2), Value::Int(-3)]);
     }
 }
