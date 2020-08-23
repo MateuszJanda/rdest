97cd3828524de30b49bd2adae77d2edfd0968394
Mateusz Janda <mateusz.janda@gmail.com>
2020-08-23 17:55:17 +0200

    Added first torrent test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 mod bencode;
+mod torrent;
 
 pub use crate::bencode::BValue;
+pub use crate::torrent::Torrent;
--- src/torrent.rs
+++ src/torrent.rs
@@ -1,20 +1,40 @@
+use crate::BValue;
 
-
+#[derive(PartialEq, Debug)]
 pub struct Torrent {
     announce : String,
     info : Info,
 
 }
 
+#[derive(PartialEq, Clone, Debug)]
 pub struct Info {
     name : String,
     piece_length : i32,
     pieces : String,
-    length : Option(i32),
-    files : Option(Vec<File>),
+    length : Option<i32>,
+    files : Option<Vec<File>>,
 }
 
+#[derive(PartialEq, Clone, Debug)]
 pub struct File {
     length : i32,
     path : Vec<String>,
+}
+
+impl Torrent {
+    pub fn from_bytes(arg: &[u8]) -> Result<Torrent, String> {
+
+        Err(format!("Nope"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_input() {
+        assert_eq!(Torrent::from_bytes(b""), Err(String::from("Nope")));
+    }
 }
