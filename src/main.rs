1ea7db3f7c4fec2e68c93bb42dacb240316516c6
Mateusz Janda <mateusz.janda@gmail.com>
2020-09-17 21:26:05 +0200

    Used reqwest::blocking

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use rdest::BValue;
 use rdest::Torrent;
+use reqwest::blocking;
 
 fn main() {
     println!("Hello, world!");
     // BValue::parse(b"i4e").unwrap();
     let t = Torrent::from_file(String::from("ubuntu-20.04.1-desktop-amd64.iso.torrent"));
-    println!("{:?}", t);
+    // println!("{:?}", t);
+
+    // get_http().await;
+    get_http();
+}
+
+// async fn get_http() -> Result<(), reqwest::Error> {
+fn get_http() -> Result<(), reqwest::Error> {
+
+    // netcat -l 127.0.0.1 8080
+    // let body = reqwest::get("http://127.0.0.1:8080")
+    //     .await?
+    //     .text()
+    //     .await?;
+
+    let body = reqwest::blocking::get("http://127.0.0.1:8080")?.text();
+    println!("body = {:?}", body);
+
+    Ok(())
 }
