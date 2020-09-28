3c6157854aca9099986b8924983b068190e02fc7
Mateusz Janda <mateusz.janda@gmail.com>
2020-09-28 22:20:12 +0200

    Creating localhost url with params

--- src/tracker_client.rs
+++ src/tracker_client.rs
@@ -30,8 +30,11 @@ impl TrackerClient {
         //     .text()
         //     .await?;
 
-        let url = metafile.url();
-        println!("url = {:?}", metafile.url());
+        let u = "http://127.0.0.1:8080/".to_string();
+        let hash : String = form_urlencoded::byte_serialize(&metafile.hash).collect();
+        let url = u + "?info_hash=" + hash.as_str();
+
+        println!("url = {:?}", url);
         println!("hash = {:?}", metafile.hash);
 
         let peer_id = rand::thread_rng()
@@ -41,51 +44,20 @@ impl TrackerClient {
 
         println!("{:?}", peer_id);
 
-        let a: &[u8] = &[49];
         let params = [
-            // ("info_hash", metafile.hash.clone()),
-            // ("info_hash", "ä"),
-            ("peer_id", "qqq"),
-            ("aaa", "123"),
-        //     ("peer_id", peer_id),
-        //     ("port", "6882".to_string()),
-        //     ("uploaded", "0".to_string()),
-        //     ("downloaded", "0".to_string()),
-        //     ("left", metafile.length().to_string()),
-        //     ("event", "started".to_string()),
-        //     // ("numwant", "50".to_string()),
-        //     ("numwant", "x x x".to_string()),
+            ("peer_id", peer_id),
+            ("port", "6882".to_string()),
+            ("uploaded", "0".to_string()),
+            ("downloaded", "0".to_string()),
+            ("left", metafile.length().to_string()),
+            ("event", "started".to_string()),
+            ("numwant", "50".to_string()),
         ];
-
-        let bytes: Vec<u8> = vec![1, 10, 100];
-        let ppp = [
-            ("www", "123"),
-        ];
-
-        let meal = &[
-            ("bread", [1, 2, 3]),
-        ];
-
-        // let encoded: String = form_urlencoded::Serializer::new(String::new())
-        //     .append_pair("foo", [1, 2, 3])
-        //     .finish();
-
-        let encoded: String = form_urlencoded::byte_serialize(&[1, 2, 3]).collect();
-        println!("{:?}", encoded);
 
         let client = reqwest::blocking::Client::new();
-
-
-        println!("{:?}", client
-            .get("http://127.0.0.1:8080?q=%20Q")
-            .form(&params)
-            .build());
-
-
         let body = client
-            .get("http://127.0.0.1:8080?q=%20Q")
-            // .form(&params)
-            .query(&ppp)
+            .get(&url)
+            .query(&params)
             .send()?
             .text();
         println!("body = {:?}", body);
