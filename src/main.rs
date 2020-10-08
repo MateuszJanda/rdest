96df489440c0502a788f22a88fde1b14966bf963
Mateusz Janda <mateusz.janda@gmail.com>
2020-10-08 21:40:23 +0200

    async loop

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,54 @@
 use rdest::{Torrent, ResponseParser};
 // use rdest::TrackerClient;
 // use hex_literal::hex;
+use tokio;
+use tokio::net::{TcpListener, TcpStream};
 
 
-fn main() {
+// fn main() {
+//     println!("Hello, world!");
+//     // let b = BValue::parse(b"i4e").unwrap();
+//     let _t = Torrent::from_file(String::from("ubuntu-20.04.1-desktop-amd64.iso.torrent"));
+//     // println!("{:?}", t);
+//
+//     // match TrackerClient::connect1(&t.unwrap()) {
+//     //     Ok(_) => println!("Http Ok"),
+//     //     Err(e) => println!("Http Problem {:?}", e),
+//     // }
+//
+//     println!("{:?}", ResponseParser::from_file("response.data".to_string()));
+// }
+
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
-    // let b = BValue::parse(b"i4e").unwrap();
-    let _t = Torrent::from_file(String::from("ubuntu-20.04.1-desktop-amd64.iso.torrent"));
-    // println!("{:?}", t);
 
-    // match TrackerClient::connect1(&t.unwrap()) {
-    //     Ok(_) => println!("Http Ok"),
-    //     Err(e) => println!("Http Problem {:?}", e),
-    // }
+    let mut listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    println!("{:?}", ResponseParser::from_file("response.data".to_string()));
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        println!("accept");
+        // process(socket).await;
+    }
+
 }
 
+
+/*
+async fn process(socket: TcpStream) {
+    // The `Connection` lets us read/write redis **frames** instead of
+    // byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        // Respond with an error
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
+}
+
+ */
