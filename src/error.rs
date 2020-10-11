5de848542ffb70e45181f1509c18d723279a14e9
Mateusz Janda <mateusz.janda@gmail.com>
2020-10-11 21:48:29 +0200

    Moved error and frame to separate files

---
+++ src/error.rs
@@ -0,0 +1,24 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Error {
+    Incomplete,
+    S(String),
+    I(std::io::Error)
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Incomplete => write!(f, "dddd"),
+            Error::S(s) => write!(f, "ssss"),
+            Error::I(i) =>  write!(f, "iii"),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Error::I(error)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,12 @@ mod response_parser;
 mod torrent;
 mod tracker_client;
 mod utils;
+mod frame;
+mod error;
 
 pub use crate::bencode::BValue;
 pub use crate::response_parser::ResponseParser;
 pub use crate::torrent::Torrent;
 pub use crate::tracker_client::TrackerClient;
+pub use crate::error::Error;
+pub use crate::frame::Frame;
