762e5d0a1e97d7c0c9e90056814de3f1a00ad9dc
Mateusz Janda <mateusz.janda@gmail.com>
2020-10-13 15:30:16 +0200

    Changed response_parser -> tracker_resp

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod bdecoder;
 mod error;
 mod frame;
-mod response_parser;
+mod tracker_resp;
 mod tracker_client;
 mod utils;
 mod metainfo;
@@ -9,6 +9,6 @@ mod metainfo;
 pub use crate::bdecoder::BValue;
 pub use crate::error::Error;
 pub use crate::frame::Frame;
-pub use crate::response_parser::ResponseParser;
+pub use crate::tracker_resp::ResponseParser;
 pub use crate::metainfo::Metainfo;
 pub use crate::tracker_client::TrackerClient;
