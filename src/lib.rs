e9157c58da37327861e2d14671125264a6caa8d8
Mateusz Janda <mateusz.janda@gmail.com>
2020-10-13 15:26:03 +0200

    Changed name torrent.rs -> metainfo.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ mod bdecoder;
 mod error;
 mod frame;
 mod response_parser;
-mod torrent;
 mod tracker_client;
 mod utils;
+mod metainfo;
 
 pub use crate::bdecoder::BValue;
 pub use crate::error::Error;
 pub use crate::frame::Frame;
 pub use crate::response_parser::ResponseParser;
-pub use crate::torrent::Torrent;
+pub use crate::metainfo::Torrent;
 pub use crate::tracker_client::TrackerClient;
