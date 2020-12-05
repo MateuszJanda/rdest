3aee236cbfe57456700cd9f81186cd9dcab43fc9
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-05 17:15:11 +0100

    Main cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,12 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    println!("Rdest");
 
-    // let mut listener = TcpListener::bind("127.0.0.1:6881").await.unwrap();
-
-    let t = Metainfo::from_file(String::from("ubuntu-20.04.1-desktop-amd64.iso.torrent")).unwrap();
-    // println!("{:?}", t);
-
-    // let r = TrackerResp::from_file("response.data".to_string()).unwrap();
-    // let r = TrackerClient::connect(&t).await.unwrap(); // TODO
-
-    // for v in r.peers {
-    //     println!("{:?}", v);
-    // }
-
-    // println!("{:?}", ResponseParser::from_file("response.data".to_string()));
-
-    let mut m = Manager::new(t, *b"ABCDEFGHIJKLMNOPQRST");
-    m.run().await;
-
-    // loop {
-    //     println!("Listening");
-    //     // The second item contains the IP and port of the new connection.
-    //     let (socket, _) = listener.accept().await.unwrap();
-    //     println!("accept");
-    //
-    //     let connection = Connection::new(socket);
-    //
-    //     let mut handler = Handler { connection };
-    //
-    //     tokio::spawn(async move {
-    //         // Process the connection. If an error is encountered, log it.
-    //         if let Err(err) = handler.run().await {
-    //             // error!(cause = ?err, "connection error");
-    //             panic!("asdf");
-    //         }
-    //     });
-    // }
+    let metainfo =
+        Metainfo::from_file(String::from("ubuntu-20.04.1-desktop-amd64.iso.torrent")).unwrap();
+    let mut manager = Manager::new(metainfo, *b"ABCDEFGHIJKLMNOPQRST");
+    manager.run().await;
 
     println!("-==[ koniec ]==-");
 }
