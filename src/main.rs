49ca0a61235c6cf614e5ff1feced59b6cde3ac68
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-07 23:21:45 +0100

    Update options

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@ use structopt::StructOpt;
 use tokio;
 
 #[derive(StructOpt)]
-struct Cli {
-    #[structopt(parse(from_os_str))]
+#[structopt(
+    name = "rdest",
+    author = "Mateusz Janda <mateusz.janda@gmail.com>",
+    about = "A simple BitTorrent Optent"
+)]
+struct Opt {
+    #[structopt(parse(from_os_str), help = "path to .torrent file")]
     path: std::path::PathBuf,
 }
 
 #[tokio::main]
 async fn main() {
-    println!("Rdest");
-
-    let args = Cli::from_args();
+    let args = Opt::from_args();
     let path = args.path;
 
     let metainfo = match Metainfo::from_file(path) {
