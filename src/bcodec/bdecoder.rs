84bb1a5a849727ca728b85e97939f76392e81f84
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-13 15:29:39 +0100

    Update docs

--- src/bcodec/bdecoder.rs
+++ src/bcodec/bdecoder.rs
@@ -1,10 +1,21 @@
 use crate::bcodec::bvalue::BValue;
 use crate::Error;
 
+/// [Bencode](https://en.wikipedia.org/wiki/Bencode) decoder used by metafile/torrent files and
+/// BitTorrent protocol.
 #[derive(PartialEq, Clone, Debug)]
 pub struct BDecoder {}
 
 impl BDecoder {
+    /// Decode [bencoded](https://en.wikipedia.org/wiki/Bencode) values.
+    ///
+    /// # Example
+    /// ```
+    /// use rdest::{BDecoder, BValue};
+    /// let val = BDecoder::from_array("i44e".as_bytes()).unwrap();
+    ///
+    /// assert_eq!(val, BValue::Int(44));
+    /// ```
     pub fn from_array(arg: &[u8]) -> Result<Vec<BValue>, Error> {
         let mut it = arg.iter().enumerate();
         BValue::values_vector(&mut it, false)
