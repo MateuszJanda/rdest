f640da66f15a2a7d7d68d218d929849163406a1d
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-13 23:04:04 +0100

    Update docs

--- src/bcodec/raw_finder.rs
+++ src/bcodec/raw_finder.rs
@@ -1,6 +1,6 @@
-/// Finder interface for dictionary key in [bencoded](https://en.wikipedia.org/wiki/Bencode) string.
+/// Dictionary value (raw format) finder in [bencoded](https://en.wikipedia.org/wiki/Bencode) string.
 pub trait RawFinder {
     /// Find first value for defined key in [bencoded](https://en.wikipedia.org/wiki/Bencode) string
-    /// with dictionaries. Value is returned in raw form.
+    /// with dictionaries. Value is returned in raw format.
     fn find_first(key: &str, arg: &[u8]) -> Option<Vec<u8>>;
 }
