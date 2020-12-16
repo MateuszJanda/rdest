543870adbb635777754d18a5a93ab34054753cbe
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-16 15:45:27 +0100

    Torrent creation called from command line

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ use tokio;
     about = "A simple BitTorrent client"
 )]
 enum Opt {
-    /// Fetch torrent files from p2p network
-    Fetch(Fetch),
+    /// Get torrent files from p2p network
+    Get(Get),
     /// Create .torrent file
     Create(Create),
 }
 
 #[derive(StructOpt)]
-struct Fetch {
+struct Get {
     /// Path to .torrent file
     #[structopt(parse(from_os_str), name = "PATH")]
     path: PathBuf,
@@ -27,8 +27,8 @@ struct Fetch {
 #[derive(StructOpt)]
 struct Create {
     /// Create .torrent for file
-    #[structopt(parse(from_os_str), name = "FILE")]
-    create: PathBuf,
+    #[structopt(parse(from_os_str), short = "-c", long = "--create", name = "FILE")]
+    path: PathBuf,
     /// Tracker address
     #[structopt(short, long, name = "ADDRESS")]
     tracker_addr: String,
@@ -36,11 +36,13 @@ struct Create {
 
 #[tokio::main]
 async fn main() {
-    let path = match Opt::from_args() {
-        Opt::Fetch(fetch) => fetch.path,
-        Opt::Create(create) => panic!("TODO"),
+    match Opt::from_args() {
+        Opt::Get(get) => get_torrent(&get.path).await,
+        Opt::Create(create) => create_torrent(&create.path, &create.tracker_addr).await,
     };
+}
 
+async fn get_torrent(path: &PathBuf) {
     let metainfo = match Metainfo::from_file(path.as_path()) {
         Ok(metainfo) => metainfo,
         Err(e) => panic!("[-] Can't read metafile. Error: {}", e),
@@ -49,4 +51,11 @@ async fn main() {
     manager.run().await;
 
     println!("-==[ koniec ]==-");
+}
+
+async fn create_torrent(path: &PathBuf, tracker_addr: &String) {
+    match Metainfo::create_file(path.as_path(), tracker_addr) {
+        Ok(()) => (),
+        Err(e) => panic!("[-] Can't create metafile. Error: {}", e),
+    }
 }
