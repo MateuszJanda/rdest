411e01bc819447bcd03c7f3458937e900e3db986
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-19 17:26:22 +0100

    Added create_pieces.py

---
+++ tests/tools/create_pieces.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import hashlib
+
+PIECE_SIZE = 2**18
+
+
+def main():
+    data = b"a" * (PIECE_SIZE + 1)
+
+    piece1 = data[:PIECE_SIZE]
+    piece2 = data[PIECE_SIZE:]
+
+    save_piece(piece1)
+    save_piece(piece2)
+
+
+def save_piece(piece):
+    h = sha1_hash(piece)
+    with open(h + ".piece", "wb") as f:
+        f.write(piece)
+
+
+def sha1_hash(piece):
+    m = hashlib.sha1()
+    m.update(piece)
+    return m.digest().hex().upper()
+
+
+if __name__ == '__main__':
+    main()
