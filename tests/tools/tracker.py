bbf50e34f8d9febb907760dda2738d7bcbb50218
Mateusz Janda <mateusz.janda@gmail.com>
2020-12-21 21:18:24 +0100

    Update tracker.py for tests

--- tests/tools/tracker.py
+++ tests/tools/tracker.py
@@ -3,7 +3,7 @@
 import http.server
 import socketserver
 
-PORT = 8001
+PORT = 8000
 
 
 class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
@@ -14,7 +14,7 @@ class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         # Whenever using 'send_header', you also have to call 'end_headers'
         self.end_headers()
 
-        html = "Hello world"
+        html = "d8:intervali1800e5:peersld 2:ip9:127.0.0.17:peer id20:AAAAABBBBBCCCCCDDDDD4:porti6881eeee"
 
         self.wfile.write(bytes(html, "utf8"))
 
@@ -22,5 +22,5 @@ class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
 
 with socketserver.TCPServer(("", PORT), HttpRequestHandler) as httpd:
-    print("Server tracker on http://0.0.0.0:%d" % PORT)
+    print("Server tracker on http://127.0.0.1:%d" % PORT)
     httpd.serve_forever()
